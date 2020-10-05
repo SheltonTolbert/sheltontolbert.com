9526b99b14bbc82a1b713848723e4731b7b8c0f3
Shelton_T <60357311+SheltonTolbert@users.noreply.github.com>
2020-10-05 13:19:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 
-from flask import Flask, render_template, url_for
+import os
+from flask import Flask, redirect, request, render_template, url_for, send_file, send_from_directory, safe_join, abort
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('about.html')
 
 
 @app.route('/home')
@@ -35,9 +37,68 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/post1')
-def post1():
-    return render_template('/blogposts/template.html')
+@app.route('/email')
+def email():
+    return render_template('email.html')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+
+# --------------- Blog posts ---------------
+
+
+@app.route('/post/subdivision')
+def subdivision():
+    return render_template('/blogposts/subdivision.html')
+
+
+@app.route('/post/lighting')
+def lighting():
+    return render_template('/blogposts/lighting.html')
+
+
+@app.route('/post/big-o')
+def bigO():
+    return render_template('/blogposts/big-o.html')
+
+
+@app.route('/post/sorting')
+def sorting():
+    return render_template('/blogposts/sorting.html')
+
+
+# -------------- Projects -------------------
+
+
+@app.route('/attraction')
+def attraction():
+    return render_template('/projects/attractor.html')
+
+# ----------------- API ---------------------
+
+
+@app.route('/api/btc', methods=['GET'])
+def backingTrackCreator():
+    if request.method == "GET":
+        try:
+
+            url = 'http://73.8.81.211:8000/api/btc?url='
+            url += request.args.get('url')
+
+            return redirect(url)
+
+        except Exception as e:
+            return str(e)
+    else:
+        return '500'
 
 
 if __name__ == '__main__':
